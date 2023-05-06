f046000ef0765e8d6edd7007e3f2a4985e94bc31
Tharindu N <tharindu.nm@yahoo.com>
2023-05-06 11:10:30 +0530

    created projects page and its components

---
+++ src/components/projects/project.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+
+import "./styles/project.css";
+
+const Project = (props) => {
+	const { logo, title, description, linkText, link } = props;
+
+	return (
+		<React.Fragment>
+			<div className="project">
+				<Link to={link}>
+					<div className="project-container">
+						<div className="project-logo">
+							<img src={logo} />
+						</div>
+						<div className="project-title">{title}</div>
+						<div className="project-description">{description}</div>
+						<div className="project-link">
+							<div className="project-link-icon">
+								<FontAwesomeIcon icon={faLink} />
+							</div>
+
+							<div className="project-link-text">{linkText}</div>
+						</div>
+					</div>
+				</Link>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Project;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,6 +3,7 @@ import React from "react";
 import NavBar from "../components/common/navBar";
 import Footer from "../components/common/footer";
 import Logo from "../components/common/logo";
+import Project from "../components/projects/project";
 
 import "./styles/projects.css";
 
@@ -21,6 +22,66 @@ const Projects = () => {
 						<div className="title projects-title">
 							Things I’ve made trying to put my dent in the
 							universe.
+						</div>
+
+						<div className="subtitle projects-subtitle">
+							I've worked on a variety of projects over the years
+							and I'm proud of the progress I've made. Many of
+							these projects are open-source and available for
+							others to explore and contribute to. If you're
+							interested in any of the projects I've worked on,
+							please feel free to check out the code and suggest
+							any improvements or enhancements you might have in
+							mind. Collaborating with others is a great way to
+							learn and grow, and I'm always open to new ideas and
+							feedback.
+						</div>
+
+						<div className="projects-list">
+							<div className="projects-project">
+								<Project
+									logo={
+										"https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png"
+									}
+									title="Portfolio"
+									description="This website is a portfolio of my work and a place for me to share my thoughts and ideas."
+									linkText="View Project"
+									link="https://github.com"
+								/>
+							</div>
+							<div className="projects-project">
+								<Project
+									logo={
+										"https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png"
+									}
+									title="Portfolio"
+									description="This website is a portfolio of my work and a place for me to share my thoughts and ideas."
+									linkText="View Project"
+									link="https://github.com"
+								/>
+							</div>
+							<div className="projects-project">
+								<Project
+									logo={
+										"https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png"
+									}
+									title="Portfolio"
+									description="This website is a portfolio of my work and a place for me to share my thoughts and ideas."
+									linkText="View Project"
+									link="https://github.com"
+								/>
+							</div>
+							<div className="projects-project">
+								<Project
+									logo={
+										"https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png"
+									}
+									title="Portfolio"
+									description="This website is a portfolio of my work and a place for me to share my thoughts and ideas."
+									linkText="View Project"
+									link="https://github.com"
+								/>
+							</div>
 						</div>
 						<div className="page-footer">
 							<Footer />
