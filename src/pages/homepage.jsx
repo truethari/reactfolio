b526fb7e9243ceee9ca334fb8b8890177ee18dc8
Tharindu N <tharindu.nm@yahoo.com>
2023-05-07 15:28:09 +0530

    added image to homepage

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -75,12 +75,28 @@ const Homepage = () => {
 					</div>
 
 					<div className="homepage-container">
-						<div className="title homepage-title">
-							{INFO.homepage.title}
-						</div>
+						<div className="homepage-first-area">
+							<div className="homepage-first-area-left-side">
+								<div className="title homepage-title">
+									{INFO.homepage.title}
+								</div>
 
-						<div className="subtitle homepage-subtitle">
-							{INFO.homepage.description}
+								<div className="subtitle homepage-subtitle">
+									{INFO.homepage.description}
+								</div>
+							</div>
+
+							<div className="homepage-first-area-right-side">
+								<div className="homepage-image-container">
+									<div className="homepage-image-wrapper">
+										<img
+											src="homepage.jpg"
+											alt="about"
+											className="homepage-image"
+										/>
+									</div>
+								</div>
+							</div>
 						</div>
 
 						<div className="homepage-socials">
