4f4ba86d6a429e127aebc5b080825f5f037bd2c4
Tharindu N <tharindu.nm@yahoo.com>
2023-05-10 11:40:15 +0530

    set title of the 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { faFaceSadTear } from "@fortawesome/free-regular-svg-icons";
@@ -6,9 +6,15 @@ import { faFaceSadTear } from "@fortawesome/free-regular-svg-icons";
 import NavBar from "../components/common/navBar";
 import Logo from "../components/common/logo";
 
+import INFO from "../data/user";
+
 import "./styles/404.css";
 
 const Notfound = () => {
+	useEffect(() => {
+		document.title = `404 | ${INFO.main.title}`;
+	}, []);
+
 	return (
 		<React.Fragment>
 			<div className="not-found page-content">
