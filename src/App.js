f657004e4836e228d96768c55e480f1c106c4c14
Tharindu N <40334396+truethari@users.noreply.github.com>
2023-05-19 08:47:59 +0530

    added google analytics 4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import ReactGA from "react-ga4";
 
 import Homepage from "./pages/homepage";
 import About from "./pages/about";
@@ -8,9 +10,14 @@ import ReadArticle from "./pages/readArticle";
 import Contact from "./pages/contact";
 import Notfound from "./pages/404";
 
+import { TRACKING_ID } from "./data/tracking";
 import "./app.css";
 
 function App() {
+	useEffect(() => {
+		ReactGA.initialize(TRACKING_ID);
+	}, []);
+
 	return (
 		<div className="App">
 			<Routes>
