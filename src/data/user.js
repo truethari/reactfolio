380092bacd45a1f0fd4535e6ef94e791141895ed
Brian Bales <48467575+brianbales99@users.noreply.github.com>
2024-05-28 12:32:31 -0700

    Homepage Intro
    Made changes to the top of the homepage but still need to fix info

--- src/data/user.js
+++ src/data/user.js
@@ -1,24 +1,22 @@
 const INFO = {
 	main: {
-		title: "Reactfolio by truethari",
-		name: "Tharindu N.",
-		email: "mail@tharindu.dev",
-		logo: "../logo.png",
+		title: "Portfolio by Brian",
+		name: "Brian Bales",
+		email: "brianbales99@gmail.com",
+		job: "Software Engineer",
+		logo: "../ProfileIMG.png",
 	},
 
 	socials: {
-		twitter: "https://twitter.com/",
-		github: "https://github.com/",
-		linkedin: "https://linkedin.com/",
-		instagram: "https://instagram.com/",
-		stackoverflow: "https://stackoverflow.com/",
-		facebook: "https://facebook.com/",
+		github: "https://github.com/brianbales99",
+		linkedin: "https://www.linkedin.com/in/brianbales99/",
+		instagram: "https://www.instagram.com/brianbales99/",
 	},
 
 	homepage: {
-		title: "Full-stack web and mobile app developer, and amateur astronaut.",
+		title: "Brian Bales ",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"I am Brian Bales, a recent graduate of California State University, Northridge, with a degree in Computer Science. Currently, I am pursuing a Master's in Software Engineering. I worked as a developer at Highstreet IT Solution for 1 year and 8 months. My research interests include machine learning and feature engineering, with a primary focus on developing a feature selection algorithm within supervised learning contexts.",
 	},
 
 	about: {
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -4,10 +4,9 @@ import { Helmet } from "react-helmet";
 import { faMailBulk } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-	faTwitter,
-	faGithub,
-	faStackOverflow,
-	faInstagram,
+    faGithub,
+    faInstagram,
+    faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
 
 import Logo from "../components/common/logo";
@@ -24,183 +23,173 @@ import myArticles from "../data/articles";
 import "./styles/homepage.css";
 
 const Homepage = () => {
-	const [stayLogo, setStayLogo] = useState(false);
-	const [logoSize, setLogoSize] = useState(80);
-	const [oldLogoSize, setOldLogoSize] = useState(80);
+    const [stayLogo, setStayLogo] = useState(false);
+    const [logoSize, setLogoSize] = useState(80);
+    const [oldLogoSize, setOldLogoSize] = useState(80);
 
-	useEffect(() => {
-		window.scrollTo(0, 0);
-	}, []);
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
 
-	useEffect(() => {
-		const handleScroll = () => {
-			let scroll = Math.round(window.pageYOffset, 2);
+    useEffect(() => {
+        const handleScroll = () => {
+            let scroll = Math.round(window.pageYOffset, 2);
 
-			let newLogoSize = 80 - (scroll * 4) / 10;
+            let newLogoSize = 80 - (scroll * 4) / 10;
 
-			if (newLogoSize < oldLogoSize) {
-				if (newLogoSize > 40) {
-					setLogoSize(newLogoSize);
-					setOldLogoSize(newLogoSize);
-					setStayLogo(false);
-				} else {
-					setStayLogo(true);
-				}
-			} else {
-				setLogoSize(newLogoSize);
-				setStayLogo(false);
-			}
-		};
+            if (newLogoSize < oldLogoSize) {
+                if (newLogoSize > 40) {
+                    setLogoSize(newLogoSize);
+                    setOldLogoSize(newLogoSize);
+                    setStayLogo(false);
+                } else {
+                    setStayLogo(true);
+                }
+            } else {
+                setLogoSize(newLogoSize);
+                setStayLogo(false);
+            }
+        };
 
-		window.addEventListener("scroll", handleScroll);
-		return () => window.removeEventListener("scroll", handleScroll);
-	}, [logoSize, oldLogoSize]);
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [logoSize, oldLogoSize]);
 
-	const currentSEO = SEO.find((item) => item.page === "home");
+    const currentSEO = SEO.find((item) => item.page === "home");
 
-	const logoStyle = {
-		display: "flex",
-		position: stayLogo ? "fixed" : "relative",
-		top: stayLogo ? "3vh" : "auto",
-		zIndex: 999,
-		border: stayLogo ? "1px solid white" : "none",
-		borderRadius: stayLogo ? "50%" : "none",
-		boxShadow: stayLogo ? "0px 4px 10px rgba(0, 0, 0, 0.25)" : "none",
-	};
+    const logoStyle = {
+        display: "flex",
+        position: stayLogo ? "fixed" : "relative",
+        top: stayLogo ? "3vh" : "auto",
+        zIndex: 999,
+        border: stayLogo ? "1px solid white" : "none",
+        borderRadius: stayLogo ? "50%" : "none",
+        boxShadow: stayLogo ? "0px 4px 10px rgba(0, 0, 0, 0.25)" : "none",
+    };
 
-	return (
-		<React.Fragment>
-			<Helmet>
-				<title>{INFO.main.title}</title>
-				<meta name="description" content={currentSEO.description} />
-				<meta
-					name="keywords"
-					content={currentSEO.keywords.join(", ")}
-				/>
-			</Helmet>
+    return (
+        <React.Fragment>
+            <Helmet>
+                <title>{INFO.main.title}</title>
+                <meta name="description" content={currentSEO.description} />
+                <meta
+                    name="keywords"
+                    content={currentSEO.keywords.join(", ")}
+                />
+            </Helmet>
 
-			<div className="page-content">
-				<NavBar active="home" />
-				<div className="content-wrapper">
-					<div className="homepage-logo-container">
-						<div style={logoStyle}>
-							<Logo width={logoSize} link={false} />
-						</div>
-					</div>
+            <div className="page-content">
+                <NavBar active="home" />
+                <div className="content-wrapper">
+                    <div className="homepage-logo-container">
+                        <div style={logoStyle}>
+                            <Logo width={logoSize} link={false} />
+                        </div>
+                    </div>
 
-					<div className="homepage-container">
-						<div className="homepage-first-area">
-							<div className="homepage-first-area-left-side">
-								<div className="title homepage-title">
-									{INFO.homepage.title}
-								</div>
+                    <div className="homepage-container">
+                        <div className="homepage-first-area">
+                            <div className="homepage-first-area-left-side">
+                                <div className="title homepage-title">
+                                    {INFO.homepage.title}
+                                    <span className="job-title"> {INFO.main.job}</span>
+                                </div>
+                                <div className="description homepage-description">
+                                    {INFO.homepage.description}
+                                </div>
+                            </div>
 
-								<div className="subtitle homepage-subtitle">
-									{INFO.homepage.description}
-								</div>
-							</div>
+                            <div className="homepage-first-area-right-side">
+                                <div className="homepage-image-container">
+                                    <div className="homepage-image-wrapper">
+                                        <img
+                                            src={INFO.main.logo}
+                                            alt="about"
+                                            className="homepage-image"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-							<div className="homepage-first-area-right-side">
-								<div className="homepage-image-container">
-									<div className="homepage-image-wrapper">
-										<img
-											src="homepage.jpg"
-											alt="about"
-											className="homepage-image"
-										/>
-									</div>
-								</div>
-							</div>
-						</div>
+                        <div className="homepage-socials">
+                            <a
+                                href={INFO.socials.github}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <FontAwesomeIcon
+                                    icon={faGithub}
+                                    className="homepage-social-icon"
+                                />
+                            </a>
+                            <a
+                                href={INFO.socials.linkedin}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <FontAwesomeIcon
+                                    icon={faLinkedin}
+                                    className="homepage-social-icon"
+                                />
+                            </a>
+                            <a
+                                href={INFO.socials.instagram}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <FontAwesomeIcon
+                                    icon={faInstagram}
+                                    className="homepage-social-icon"
+                                />
+                            </a>
+                            <a
+                                href={`mailto:${INFO.main.email}`}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <FontAwesomeIcon
+                                    icon={faMailBulk}
+                                    className="homepage-social-icon"
+                                />
+                            </a>
+                        </div>
 
-						<div className="homepage-socials">
-							<a
-								href={INFO.socials.twitter}
-								target="_blank"
-								rel="noreferrer"
-							>
-								<FontAwesomeIcon
-									icon={faTwitter}
-									className="homepage-social-icon"
-								/>
-							</a>
-							<a
-								href={INFO.socials.github}
-								target="_blank"
-								rel="noreferrer"
-							>
-								<FontAwesomeIcon
-									icon={faGithub}
-									className="homepage-social-icon"
-								/>
-							</a>
-							<a
-								href={INFO.socials.stackoverflow}
-								target="_blank"
-								rel="noreferrer"
-							>
-								<FontAwesomeIcon
-									icon={faStackOverflow}
-									className="homepage-social-icon"
-								/>
-							</a>
-							<a
-								href={INFO.socials.instagram}
-								target="_blank"
-								rel="noreferrer"
-							>
-								<FontAwesomeIcon
-									icon={faInstagram}
-									className="homepage-social-icon"
-								/>
-							</a>
-							<a
-								href={`mailto:${INFO.main.email}`}
-								target="_blank"
-								rel="noreferrer"
-							>
-								<FontAwesomeIcon
-									icon={faMailBulk}
-									className="homepage-social-icon"
-								/>
-							</a>
-						</div>
+                        <div className="homepage-projects">
+                            <AllProjects />
+                        </div>
 
-						<div className="homepage-projects">
-							<AllProjects />
-						</div>
+                        <div className="homepage-after-title">
+                            <div className="homepage-articles">
+                                {myArticles.map((article, index) => (
+                                    <div
+                                        className="homepage-article"
+                                        key={(index + 1).toString()}
+                                    >
+                                        <Article
+                                            key={(index + 1).toString()}
+                                            date={article().date}
+                                            title={article().title}
+                                            description={article().description}
+                                            link={"/article/" + (index + 1)}
+                                        />
+                                    </div>
+                                ))}
+                            </div>
 
-						<div className="homepage-after-title">
-							<div className="homepage-articles">
-								{myArticles.map((article, index) => (
-									<div
-										className="homepage-article"
-										key={(index + 1).toString()}
-									>
-										<Article
-											key={(index + 1).toString()}
-											date={article().date}
-											title={article().title}
-											description={article().description}
-											link={"/article/" + (index + 1)}
-										/>
-									</div>
-								))}
-							</div>
+                            <div className="homepage-works">
+                                <Works />
+                            </div>
+                        </div>
 
-							<div className="homepage-works">
-								<Works />
-							</div>
-						</div>
-
-						<div className="page-footer">
-							<Footer />
-						</div>
-					</div>
-				</div>
-			</div>
-		</React.Fragment>
-	);
+                        <div className="page-footer">
+                            <Footer />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
 };
 
 export default Homepage;
